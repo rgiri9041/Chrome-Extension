d8ad0299a7770450a9f7b84ce9377a0207e36f0e
Raj Kumar Giri <rgiri9041@gmail.com>
2026-05-25 11:23:11 -0500

    converted the inpute text into link and when it is clicked it opens in new

--- script.js
+++ script.js
@@ -6,23 +6,29 @@ let ulEl = document.querySelector("#ul-el")
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    inputEl.value = "" // to clear the input value in the box
     renderList()
+    inputEl.value = "" // to clear the input value in the box
      
 })
  
 function renderList (){
-    let listItem = ""
 
-    for(let i = 0; i < myLeads.length; i++){ 
-    listItem += "<li>" + myLeads[i]+ "</li>"
-    // Other way to fetch the input text from the html in inorder list
+    let listItem = `<li>
+                        <a target="_blank" href= "${inputEl.value}"> ${inputEl.value} </a>
+                    </li>`
+    ulEl.innerHTML += listItem
+//This can be done without using loop as well
+    // let listItem = ""
+
+    // for(let i = 0; i < myLeads.length; i++){ 
+    // listItem += "<li>" + myLeads[i]+ "</li>"
+// Other way to fetch the input text from the html in inorder list
    // let li = document.createElement("li")
    // li.textContent = myLeads[i]
    // ulEl.append(li)
 }
     ulEl.innerHTML = listItem
 
-}
+
 
 
